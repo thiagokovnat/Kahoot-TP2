d0aa12b8a573590ca238db6d4a353c6e4756cbc1
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2020-07-20 00:34:10 -0300

    Clase SistemaDePuntuacion añadida junto con el método public void puntuarRespuestaCorrecta(...) que puntua correspondientemente al jugador seleccionado

---
+++ src/main/java/edu/fiuba/algo3/modelo/SistemaDePuntuacion/SistemaDePuntuacion.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.modelo.SistemaDePuntuacion;
+import edu.fiuba.algo3.modelo.Jugador.Jugador;
+
+public class SistemaDePuntuacion {
+
+    public void puntuarRespuestaCorrecta(Jugador unJugador){
+        unJugador.modificarPuntos(1);
+    }
+
+}
