4d1282d4a6ec993af19447b6d17089768adb4ce5
lucassleoni <lucas-s-leoni@hotmail.com>
2020-07-21 17:24:48 -0300

    Cambio 'public' por 'private' en los atributos de Respuesta.

--- src/main/java/edu/fiuba/algo3/modelo/Respuesta/Respuesta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Respuesta/Respuesta.java
@@ -4,11 +4,10 @@ import edu.fiuba.algo3.modelo.Jugador.Jugador;
 
 public class Respuesta{
 
-    public Jugador jugador;
-    public String opcionElegida;
+    private Jugador jugador;
+    private String opcionElegida;
 
     public Respuesta( Jugador unJugador, String opcionElegida ){
-
         this.jugador = unJugador;
         this.opcionElegida = opcionElegida;
     }
