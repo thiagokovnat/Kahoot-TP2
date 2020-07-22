862f64425736778cb2dcc397ebd90795ee0646cf
Thiago Kovnat <49885922+thiagokovnat@users.noreply.github.com>
2020-07-22 01:37:29 -0300

    Fix: Arreglo visibilidad de los atributos de respuesta

--- src/main/java/edu/fiuba/algo3/modelo/Respuesta/Respuesta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Respuesta/Respuesta.java
@@ -4,8 +4,8 @@ import edu.fiuba.algo3.modelo.Jugador.Jugador;
 
 public class Respuesta{
 
-    private Jugador jugador;
-    private String opcionElegida;
+    public Jugador jugador;
+    public String opcionElegida;
 
     public Respuesta( Jugador unJugador, String opcionElegida ){
         this.jugador = unJugador;
