523e8186157757211fa740831a9111a303c0a4b4
thiagokovnat <tkovnat@fi.uba.ar>
2020-07-29 17:22:06 -0300

    Override al metodo equals de Opcion

--- src/main/java/edu/fiuba/algo3/modelo/Opcion/Opcion.java
+++ src/main/java/edu/fiuba/algo3/modelo/Opcion/Opcion.java
@@ -2,15 +2,17 @@ package edu.fiuba.algo3.modelo.Opcion;
 
 public class Opcion {
     private String texto;
+    private String grupo;
 
     public Opcion(String texto){
 
         this.texto = texto;
+        this.grupo = null;
     }
 
     public boolean textoEsIgual(String texto){
 
-        return (this.texto == texto);
+        return (this.texto.equals(texto));
     }
 
     public boolean esIgualA(Opcion unaOpcion){
@@ -21,5 +23,18 @@ public class Opcion {
     public String getTexto(){
 
         return this.texto;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == null || (obj.getClass() != this.getClass()))
+            return false;
+
+        if(this == obj)
+            return true;
+
+        Opcion opc = (Opcion) obj;
+
+        return (this.texto.equals(opc.texto) && (this.grupo == null || this.grupo.equals(opc.grupo)));
     }
 }
