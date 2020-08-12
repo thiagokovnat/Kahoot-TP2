aac7634f94613c869554172e9bfc28152bec9c8a
SantiagoLocatelli <santilocatelli@hotmail.com>
2020-08-12 15:26:43 -0300

    Agregado de la clase Ronda
    Co-authored-by: thiagokovnat <49885922+thiagokovnat@users.noreply.github.com>

---
+++ src/main/java/edu/fiuba/algo3/modelo/Ronda/Ronda.java
@@ -0,0 +1,55 @@
+package edu.fiuba.algo3.modelo.Ronda;
+
+import edu.fiuba.algo3.modelo.Exceptions.CantidadMaximaDeJugadoresSuperadaException;
+import edu.fiuba.algo3.modelo.Exceptions.CantidadUsoMultiplicadorExcedidoException;
+import edu.fiuba.algo3.modelo.Exceptions.PreguntaNoAdmiteMultiplicadorException;
+import edu.fiuba.algo3.modelo.Jugador.Jugador;
+import edu.fiuba.algo3.modelo.Multiplicador.Multiplicador;
+import edu.fiuba.algo3.modelo.Opcion.Opcion;
+import edu.fiuba.algo3.modelo.Pregunta.Pregunta;
+import edu.fiuba.algo3.modelo.Respuesta.Respuesta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Ronda {
+    public Pregunta pregunta;
+    public List<Jugador> jugadores = new ArrayList<>();
+    public ListIterator<Jugador> iterador;
+    public List<Respuesta> respuestas = new ArrayList<>();
+    public Jugador jugadorActual;
+
+    public Ronda( Pregunta pregunta, List<Jugador> jugadores ){
+
+        this.pregunta = pregunta;
+        this.jugadores.addAll(jugadores);
+        this.iterador = this.jugadores.listIterator();
+        this.jugadorActual = iterador.next();
+    }
+
+    public void finalizar(){
+        this.pregunta.puntuarRespuestas(respuestas);
+    }
+
+    public void responder(List<Opcion> opcionesElegidas ){
+        Respuesta respuesta = jugadorActual.responder(opcionesElegidas);
+        respuestas.add(respuesta);
+    }
+
+    public void activarMultiplicador(Multiplicador multiplicador) throws CantidadUsoMultiplicadorExcedidoException, PreguntaNoAdmiteMultiplicadorException {
+
+        jugadorActual.activarMultiplicador(multiplicador, this.pregunta);
+    }
+
+    // Devuelve true si sigue habiendo jugadores para que respondan, caso contrario false.
+    public boolean proximoTurno(){
+        if( this.iterador.hasNext() ) {
+            this.jugadorActual = this.iterador.next();
+            return true;
+        }else{
+            finalizar();
+            return false;
+        }
+    }
+}
